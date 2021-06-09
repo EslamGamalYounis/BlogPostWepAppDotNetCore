f78e75161ddd9ff91697c64384ad5809c4ce9aeb
Islam Gamal <eslam.gamal.younis@gmail.com>
2021-06-09 03:44:38 +0200

    add default root

--- TaskImaging/Startup.cs
+++ TaskImaging/Startup.cs
@@ -24,7 +24,7 @@ namespace TaskImaging
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(configure => configure.Conventions.AddPageRoute("/BlogPostPages/GetAllBlogPosts", ""));
             services.AddDbContext<BlogContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BlogContext")));
         }
 
